1c69345ef348ae1eb38debcbcc357f3767e5c363
Michael Thomas <michaelthomas372@gmail.com>
2024-01-15 17:24:11 +0100

    update remember me url

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Wednesday, January 10, 2024 @ 07:10:55 ET
+ *  Date: Monday, January 15, 2024 @ 11:23:52 ET
  *  By: michael
  *  ENGrid styles: v0.16.4
  *  ENGrid scripts: v0.16.10
@@ -21781,7 +21781,7 @@ const options = {
 if (document.body.dataset.engridTheme === "nwf2") {
   options.RememberMe = {
     checked: true,
-    remoteUrl: "https://www.nwf.org/remember_me/",
+    remoteUrl: "https://rememberme.nwf.org/index.html",
     fieldOptInSelectorTarget: "div.en__field--phoneNumber2, div.en__field--phoneNumber, div.en__field--email",
     fieldOptInSelectorTargetLocation: "after",
     fieldClearSelectorTarget: "div.en__field--email div",
