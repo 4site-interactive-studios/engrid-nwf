97c25e71d645cd7c6d8138f3be95752b03c82767
Fernando Santos <fern@ndo.io>
2025-11-05 23:02:22 -0500

    ENgrid Update

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,10 +17,10 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Monday, November 3, 2025 @ 16:23:47 ET
+ *  Date: Wednesday, November 5, 2025 @ 23:02:08 ET
  *  By: fernando
  *  ENGrid styles: v0.23.0
- *  ENGrid scripts: v0.23.0
+ *  ENGrid scripts: v0.23.1
  *
  *  Created by 4Site Studios
  *  Come work with us or join our team, we would love to hear from you
@@ -31878,6 +31878,11 @@ class preferred_payment_method_PreferredPaymentMethod {
     if (engrid_ENGrid.getPageType() !== "DONATION") {
       this.logger.log("Not a donation page. Skipping preferred payment selection.");
       return false;
+    } // If there's a "payment" URL parameter, we can proceed
+
+
+    if (engrid_ENGrid.getUrlParameter("payment")) {
+      return true;
     }
 
     if (!this.getGiveBySelectInputs().length) {
@@ -31885,7 +31890,7 @@ class preferred_payment_method_PreferredPaymentMethod {
       return false;
     }
 
-    const config = engrid_ENGrid.getOption("PreferredPaymentMethod");
+    const config = engrid_ENGrid.getOption("PreferredPaymentMethod") || false;
 
     if (config === false) {
       this.logger.log("PreferredPaymentMethod option disabled.");
@@ -31896,7 +31901,7 @@ class preferred_payment_method_PreferredPaymentMethod {
   }
 
   resolveConfig() {
-    const option = engrid_ENGrid.getOption("PreferredPaymentMethod");
+    const option = engrid_ENGrid.getOption("PreferredPaymentMethod") || false;
 
     if (option && typeof option === "object") {
       const preferredPaymentMethodField = option.preferredPaymentMethodField || "";
@@ -32096,18 +32101,11 @@ class preferred_payment_method_PreferredPaymentMethod {
       return;
     }
 
-    const label = this.findLabelForInput(input);
-
-    if (label) {
-      label.click();
-    } else {
-      input.checked = true;
-      input.dispatchEvent(new Event("change", {
-        bubbles: true,
-        cancelable: true
-      }));
-    }
-
+    input.checked = true;
+    input.dispatchEvent(new Event("change", {
+      bubbles: true,
+      cancelable: true
+    }));
     engrid_ENGrid.setPaymentType(method);
     this.syncPreferredField(input.value);
     this.selectionFinalized = true;
@@ -32184,7 +32182,7 @@ class preferred_payment_method_PreferredPaymentMethod {
 
 }
 ;// CONCATENATED MODULE: ../engrid/packages/scripts/dist/version.js
-const version_AppVersion = "0.23.0";
+const version_AppVersion = "0.23.1";
 ;// CONCATENATED MODULE: ../engrid/packages/scripts/dist/index.js
  // Runs first so it can change the DOM markup before any markup dependent code fires
 
@@ -45299,11 +45297,15 @@ class dist_preferred_payment_method_PreferredPaymentMethod {
             this.logger.log("Not a donation page. Skipping preferred payment selection.");
             return false;
         }
+        // If there's a "payment" URL parameter, we can proceed
+        if (ENGrid.getUrlParameter("payment")) {
+            return true;
+        }
         if (!this.getGiveBySelectInputs().length) {
             this.logger.log("No give-by-select inputs found. Skipping.");
             return false;
         }
-        const config = ENGrid.getOption("PreferredPaymentMethod");
+        const config = ENGrid.getOption("PreferredPaymentMethod") || false;
         if (config === false) {
             this.logger.log("PreferredPaymentMethod option disabled.");
             return false;
@@ -45311,7 +45313,7 @@ class dist_preferred_payment_method_PreferredPaymentMethod {
         return true;
     }
     resolveConfig() {
-        const option = ENGrid.getOption("PreferredPaymentMethod");
+        const option = ENGrid.getOption("PreferredPaymentMethod") || false;
         if (option && typeof option === "object") {
             const preferredPaymentMethodField = option.preferredPaymentMethodField || "";
             const defaultPaymentMethod = Array.isArray(option.defaultPaymentMethod)
@@ -45485,14 +45487,8 @@ class dist_preferred_payment_method_PreferredPaymentMethod {
             this.logger.log(`Payment method "${method}" is not available to select.`);
             return;
         }
-        const label = this.findLabelForInput(input);
-        if (label) {
-            label.click();
-        }
-        else {
-            input.checked = true;
-            input.dispatchEvent(new Event("change", { bubbles: true, cancelable: true }));
-        }
+        input.checked = true;
+        input.dispatchEvent(new Event("change", { bubbles: true, cancelable: true }));
         ENGrid.setPaymentType(method);
         this.syncPreferredField(input.value);
         this.selectionFinalized = true;
