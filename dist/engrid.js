5d24e3be9e3b7911e00d8f18f7f2d868ab4bb0f5
Fernando Santos <fern@ndo.io>
2026-03-18 22:08:13 -0300

    Hide OneTrust on donation thank-you embed

--- dist/engrid.js
+++ dist/engrid.js
@@ -17,7 +17,7 @@
  *
  *  ENGRID PAGE TEMPLATE ASSETS
  *
- *  Date: Wednesday, March 18, 2026 @ 18:16:44 ET
+ *  Date: Wednesday, March 18, 2026 @ 21:07:43 ET
  *  By: fernando
  *  ENGrid styles: v0.24.0
  *  ENGrid scripts: v0.24.3
