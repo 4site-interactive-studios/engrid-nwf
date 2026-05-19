818096156521f0a8c0d9623b21a33f82dcb58e18
Michael Thomas <9821783+MichaelT372@users.noreply.github.com>
2026-05-19 19:30:25 +0200

    upstream opt in ladder edits

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import {
-  Options,
-  App,
-  DonationFrequency,
-  EnForm,
-  OptInLadder,
-} from "@4site/engrid-scripts"; // Uses ENGrid via NPM
 // import {
 //   Options,
 //   App,
 //   DonationFrequency,
 //   EnForm,
 //   OptInLadder,
-// } from "../../engrid/packages/scripts"; // Uses ENGrid via Visual Studio Workspace
+// } from "@4site/engrid-scripts"; // Uses ENGrid via NPM
+import {
+  Options,
+  App,
+  DonationFrequency,
+  EnForm,
+  OptInLadder,
+} from "../../engrid/packages/scripts"; // Uses ENGrid via Visual Studio Workspace
 import "./sass/main.scss";
 import { customScript } from "./scripts/main";
 import { FormSwitch } from "./scripts/form-switch/form-switch";
